ba8c13e44d99eb0b0639378862f81b3c840c1f01
Glyph <glyph@twistedmatrix.com>
2022-01-21 00:55:42 -0800

    add time-scaling

--- src/fritter/recursive_driver.py
+++ src/fritter/recursive_driver.py
@@ -4,15 +4,18 @@ from typing import Callable, Optional, Tuple
 from .scheduler import CallHandle, Scheduler
 
 
-
 @dataclass
 class RecursiveDriver(object):
     _parent: Scheduler
     _call: Optional[CallHandle[float]] = None
-    _offset: float = 0  # relative to parent's timestamp
+    _offset: float = 0  # amount to subtract from parent's timestamp to get to
+    # this driver's relative timestamp - in parent's
+    # (unscaled) time-scale
     _running: bool = False
-    _pauseTime: float = 0  # *local* (not parent's) timestamp at which we were paused
+    _pauseTime: float = 0.0  # *local* (not parent's) timestamp at which we were paused
     _toRunWhenStarted: Optional[Tuple[float, Callable[[], None]]] = None
+    _scaleFactor: float = 1.0  # how much faster the local time coordinate
+    # system is within this scheduler.
 
     def reschedule(self, desiredTime: float, work: Callable[[], None]):
         def _():
@@ -24,7 +27,9 @@ class RecursiveDriver(object):
             self._call.cancel()
         self._toRunWhenStarted = desiredTime, work
         if self._running:
-            self._call = self._parent.callAtTimestamp(desiredTime + self._offset, _)
+            self._call = self._parent.callAtTimestamp(
+                (desiredTime / self._scaleFactor) + self._offset, _
+            )
         else:
             assert (
                 self._call is None
@@ -38,24 +43,51 @@ class RecursiveDriver(object):
 
     def currentTimestamp(self) -> float:
         if self._running:
-            return self._parent.currentTimestamp() - self._offset
+            return (self._parent.currentTimestamp() - self._offset) * self._scaleFactor
         else:
             return self._pauseTime
 
     # |                       |
     # v recursive driver only v
 
-    def start(self):
-        self._offset = self._scheduler.currentTimestamp() + self._pauseTime
+    def start(self) -> None:
+        if self._running:
+            return
+        self._offset = self._scheduler.currentTimestamp() + (
+            self._pauseTime / self._scaleFactor
+        )
         self._pauseTime = 0
         self._running = True
         rws = self._toRunWhenStarted
         if rws is not None:
             self.reschedule(*rws)
 
-    def pause(self):
+    def pause(self) -> None:
         self._pauseTime = self.currentTimestamp()
         self._running = False
         if self._call is not None:
             self._call.cancel()
             self._call = None
+
+    @property
+    def scaleFactor(self):
+        """
+        The scale factor is how much faster than its parent time passes in this driver.
+        """
+        return self._scaleFactor
+
+    @scaleFactor.setter
+    def _setScaleFactor(self, newScaleFactor: float) -> None:
+        """
+        Change this recursive driver to be running at `newScaleFactor` times
+        its parent scheduler's rate.  i.e. driver.changeScaleFactor(3.0) will
+        change this driver's rate of time passing to be 3x faster than its
+        parent.
+        """
+        wasRunning = self._running
+        if wasRunning:
+            self.pause()
+            self._scaleFactor = newScaleFactor
+            self.start()
+        else:
+            self._scaleFactor = newScaleFactor
