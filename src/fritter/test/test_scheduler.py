8c8236edf3c564a3c5e9103dce0d0247ed9fde46
Glyph <glyph@twistedmatrix.com>
2022-01-23 18:34:37 -0800

    test for cancellation

--- src/fritter/test/test_scheduler.py
+++ src/fritter/test/test_scheduler.py
@@ -1,8 +1,10 @@
+from typing import Callable
 from unittest import TestCase
 
-from ..scheduler import Scheduler
-from ..priority_queue import HeapPriorityQueue
 from ..memory_driver import MemoryDriver
+from ..priority_queue import HeapPriorityQueue
+from ..scheduler import Scheduler
+
 
 class SchedulerTests(TestCase):
     """
@@ -26,3 +28,33 @@ class SchedulerTests(TestCase):
         self.assertEqual(0, called)
         driver.advance(2.0)
         self.assertEqual(1, called)
+
+    def test_canceling(self):
+        """ """
+        scheduler = Scheduler(HeapPriorityQueue(), driver := MemoryDriver())
+        callTimes = []
+
+        def record(event: str) -> Callable[[], None]:
+            def result():
+                callTimes.append((scheduler.currentTimestamp(), event))
+            return result
+
+        scheduler.callAtTimestamp(1.0, record("a"))
+        bHandle = scheduler.callAtTimestamp(2.0, record("b"))
+        scheduler.callAtTimestamp(3.0, record("c"))
+        didCancel = []
+
+        def bCancel() -> None:
+            didCancel.append(True)
+            bHandle.cancel()
+
+        scheduler.callAtTimestamp(1.5, bCancel)
+        self.assertEqual(callTimes, [])
+        driver.advance()
+        self.assertEqual(callTimes, [(1.0, "a")])
+        self.assertEqual(didCancel, [])
+        driver.advance()
+        self.assertEqual(callTimes, [(1.0, "a")])
+        self.assertEqual(didCancel, [True])
+        driver.advance()
+        self.assertEqual(callTimes, [(1.0, "a"), (3.0, "c")])
