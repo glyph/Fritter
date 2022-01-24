f62741b6f141bc612a1161ca4c2ededbf83d4e07
Glyph <glyph@twistedmatrix.com>
2022-01-23 19:28:36 -0800

    more mypy

--- src/fritter/test/test_tree.py
+++ src/fritter/test/test_tree.py
@@ -1,13 +1,14 @@
+from typing import List, Tuple
 from unittest import TestCase
 
-from ..recursive_driver import RecursiveDriver
 from ..memory_driver import MemoryDriver
 from ..priority_queue import HeapPriorityQueue
+from ..recursive_driver import RecursiveDriver
 from ..scheduler import Scheduler
 
 
 class RecursiveTest(TestCase):
-    def _oneRecursiveCall(self, scaleFactor: float) -> None:
+    def _oneRecursiveCall(self, scaleFactor: float) -> List[Tuple[float, float]]:
         scheduler1 = Scheduler(HeapPriorityQueue(), driver := MemoryDriver())
         scheduler2 = Scheduler(
             HeapPriorityQueue(), recursive := RecursiveDriver(scheduler1)
@@ -24,10 +25,10 @@ class RecursiveTest(TestCase):
         driver.advance()
         return calls
 
-    def test_scaling(self):
+    def test_scaling(self) -> None:
         calls = self._oneRecursiveCall(1.0)
         self.assertEqual(calls, [(1.0, 1.0)])
         calls = self._oneRecursiveCall(3.0)
-        self.assertEqual(calls, [(1/3., 1.0)])
-        calls = self._oneRecursiveCall(1/3.)
+        self.assertEqual(calls, [(1 / 3.0, 1.0)])
+        calls = self._oneRecursiveCall(1 / 3.0)
         self.assertEqual(calls, [(3.0, 1.0)])
