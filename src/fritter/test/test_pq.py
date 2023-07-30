6adbc7dfb7ca81478d87e269c2f869febe74bded
Glyph <code@glyph.im>
2023-07-30 10:01:56 -0700

    priority queue test coverage

---
+++ src/fritter/test/test_pq.py
@@ -0,0 +1,30 @@
+from fritter.priority_queue import HeapPriorityQueue
+
+from unittest import TestCase
+
+class QueueTests(TestCase):
+    def setUp(self) -> None:
+        self.q: HeapPriorityQueue[int] = HeapPriorityQueue()
+
+    def test_get(self) -> None:
+        self.q.add(3)
+        self.q.add(4)
+        self.q.add(2)
+        self.assertEqual(self.q.get(), 2)
+        self.assertEqual(self.q.get(), 3)
+        self.assertEqual(self.q.get(), 4)
+        self.assertIs(self.q.get(), None)
+
+    def test_peek(self) -> None:
+        self.q.add(7)
+        self.assertEqual(self.q.peek(), 7)
+        self.assertEqual(self.q.peek(), 7)
+
+    def test_remove(self) -> None:
+        self.q.add(9)
+        self.q.add(10)
+        self.q.add(11)
+        self.assertEqual(self.q.remove(70), False)
+        self.assertEqual(self.q.remove(10), True)
+        self.assertEqual(self.q.get(), 9)
+        self.assertEqual(self.q.get(), 11)
