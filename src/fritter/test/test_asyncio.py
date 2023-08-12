0b7af7fcaf906e2b0f8c5fc2cab37156f6f37ac7
Glyph <code@glyph.im>
2023-08-12 16:27:55 -0700

    cancel

--- src/fritter/test/test_asyncio.py
+++ src/fritter/test/test_asyncio.py
@@ -1,18 +1,37 @@
-from asyncio.exceptions import InvalidStateError
+from asyncio.exceptions import InvalidStateError, CancelledError
 from unittest import TestCase
 
 from ..drivers.asyncio import AsyncioAsyncDriver
 
 
 class AsyncDriverTests(TestCase):
-    def test_newWithCancel(self) -> None:
+    def setUp(self) -> None:
+        self.called = 0
+
+    def call(self) -> None:
+        self.called += 1
+
+    def test_complete(self) -> None:
         driver = AsyncioAsyncDriver()
 
         def cancel() -> None:
             pass
 
-        f = driver.newWithCancel(cancel)
+        f = driver.newWithCancel(self.call)
         with self.assertRaises(InvalidStateError):
             f.result()
+
         driver.complete(f)
         self.assertIsNone(f.result())
+
+    def test_cancel(self) -> None:
+        driver = AsyncioAsyncDriver()
+
+        f = driver.newWithCancel(self.call)
+        with self.assertRaises(InvalidStateError):
+            f.result()
+        f.cancel()
+        with self.assertRaises(CancelledError):
+            f.result()
+
+
