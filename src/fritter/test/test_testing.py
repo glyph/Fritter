c1a04843d1c8be3a3645b122e8d6f0ca95d20b29
Glyph <code@glyph.im>
2026-02-05 21:35:45 -0800

    test docstrings that explain everything we're doing here
    This commit was sponsored by Mark Harviston, James Harley Gorrell,
    hacklschorsch, and my other patrons.  If you want to join them, you
    can support my work at https://glyph.im/patrons/.

--- src/fritter/test/test_testing.py
+++ src/fritter/test/test_testing.py
@@ -7,7 +7,20 @@ epsilon = 1e-100
 
 
 class MemoryDriverTests(TestCase):
+    """
+    Tests for L{MemoryDriver}.
+    """
     def test_advance(self) -> None:
+        """
+        L{MemoryDriver.advance} advances its internal clock adn runs its
+        scheduled work.
+
+            - when given a value, it advances by an amount of time represented
+              by that value
+
+            - when given no value, it advances to the time of the scheduled
+              work.
+        """
         driver = MemoryDriver()
         work = []
         self.assertEqual(driver.isScheduled(), False)
@@ -22,6 +35,10 @@ class MemoryDriverTests(TestCase):
         self.assertEqual(driver.advance(), None)
 
     def test_noBackwards(self) -> None:
+        """
+        When work is scheduled in the past, time on L{MemoryDriver.advance}
+        does not move backwards, but advances by a small epsilon.
+        """
         driver = MemoryDriver()
         count = 0
 
@@ -37,6 +54,10 @@ class MemoryDriverTests(TestCase):
         self.assertLess(driver.now(), 1e-20)
 
     def test_step(self) -> None:
+        """
+        L{MemoryDriver.step} will invoke the drivers's work its invocations
+        before returning limited, by default, to 100.
+        """
         driver = MemoryDriver()
         self.assertEqual(driver.isScheduled(), False)
         count = 0
@@ -52,7 +73,13 @@ class MemoryDriverTests(TestCase):
         self.assertEqual(count, 100)
         self.assertTrue(driver.now() < epsilon)
 
-    def test_step2(self) -> None:
+    def test_stepNowAdvancesSynchronously(self) -> None:
+        """
+        L{MemoryDriver.step} will limit its number of synchronous invocations
+        to a limit described by its C{maxCalls} parameter.  Each invocation
+        will have C{driver.now()} set to the time that was passed to
+        L{MemoryDriver.reschedule}.
+        """
         driver = MemoryDriver()
         count = 0
         nows = []
@@ -73,19 +100,31 @@ class MemoryDriverTests(TestCase):
         self.assertEqual(driver.now(), 110.0)
         self.assertEqual(nows, [10.0, 30.0, 50.0, 70.0, 90.0, 110.0])
 
-    def test_step0(self) -> None:
+    def test_stepUntil(self) -> None:
+        """
+        L{MemoryDriver.step} takes an C{until} parameter which describes an
+        upper limit of the time that it will advance the driver's C{.now()} to.
+        """
         driver = MemoryDriver()
         steps = driver.step(until=5.0)
         self.assertEqual(steps, 0)
         self.assertEqual(driver.now(), 5.0)
 
-    def test_step00(self) -> None:
+    def test_stepNothing(self) -> None:
+        """
+        If no work is scheduled, L{MemoryDriver.step} does nothing, not
+        advancing time at all.
+        """
         driver = MemoryDriver()
         steps = driver.step()
         self.assertEqual(steps, 0)
         self.assertEqual(driver.now(), 0.0)
 
-    def test_step1(self) -> None:
+    def test_stepOnce(self) -> None:
+        """
+        If a callable doesn't reschedule itself, L{MemoryDriver.step} will run
+        it once and advance to its time.
+        """
         driver = MemoryDriver()
         count = 0
 
@@ -99,7 +138,12 @@ class MemoryDriverTests(TestCase):
         self.assertEqual(count, 1)
         self.assertEqual(driver.now(), 10.0)
 
-    def test_step_early(self) -> None:
+    def test_stepMuchEarlier(self) -> None:
+        """
+        If a callable scheduled with L{MemoryDriver.reschedule} calls
+        C{reschedule} again with a time far in the past, L{MemoryDriver.step}
+        will advance time by a tiny epsilon and not move time backwards.
+        """
         driver = MemoryDriver()
         count = 0
 
@@ -115,7 +159,15 @@ class MemoryDriverTests(TestCase):
         self.assertGreater(driver.now(), 10.0)
         self.assertLess(10 - driver.now(), epsilon)
 
-    def test_discrete_driver(self) -> None:
+    def test_discreteDriver(self) -> None:
+        """
+        L{DiscreteDriver} wraps a time driver and a L{MemoryDriver}, and
+        simulates a discrete time simulation by remembering the I{scheduled}
+        time of each callable as it runs, as its value for
+        L{DiscreteDriver.now}, thus making it possible for code to know what
+        time it was I{supposed} to be run at (i.e. when its discrete invocation
+        time was) rather than what it was actually invoked.
+        """
         pretendReal = MemoryDriver()
         discrete = DiscreteDriver(pretendReal)
         count = 0
@@ -132,7 +184,12 @@ class MemoryDriverTests(TestCase):
         self.assertEqual(when, [3.0, 6.0, 9.0])
         self.assertEqual(count, 3)
 
-    def test_continuous_driver(self) -> None:
+    def test_continuousDriver(self) -> None:
+        """
+        As opposed to L{MemoryDriver.step}, L{MemoryDriver.advance} advances
+        the value of C{.now()} immediately, so that each invoked callable will
+        see that "real" time rather than the time they were scheduled at.
+        """
         continuous = MemoryDriver()
         count = 0
         when = []
