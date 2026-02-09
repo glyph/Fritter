92ef9510cf2ae9f066461248c5fbc73c5fa7f6de
Glyph <code@glyph.im>
2026-02-08 17:20:51 -0800

    SIGH. ONE DAY
    This commit was sponsored by abliss, Geoffrey Thomas, Jason Mills, and
    my other patrons.  If you want to join them, you can support my work
    at https://glyph.im/patrons/.

--- src/fritter/drivers/memory.py
+++ src/fritter/drivers/memory.py
@@ -13,11 +13,16 @@ from typing import (
     Optional,
     Protocol,
     Tuple,
-    Self,
     TYPE_CHECKING,
     TypeVar,
     Generic,
 )
+import sys
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 from fritter.boundaries import PriorityComparable
 
